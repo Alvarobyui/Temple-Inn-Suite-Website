a3296ad139e98a1a679614f083d040161ecc6462
Your Name <you@example.com>
2022-04-06 15:57:13 -0500

    Reserve done

--- js/temples.js
+++ js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://github.com/Alvarobyui/wdd230/blob/main/chamber/json/data.json';
+const requestURL = 'https://github.com/Alvarobyui/Final-Project-WDD230/blob/main/json/temple-info.json';
 const cards = document.querySelector('.directory');
 
 
@@ -7,31 +7,13 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const companies = jsonObject['directory'];  
+    //const companies = jsonObject['directory'];  
     //console.log(JSON.parse(userJson));
     console.table(JSON.parse(jsonObject));  // temporary checking for valid response and data parsing
-    companies.forEach(displayCompany);
+    //temple.forEach(displayTemple);
   }); 
-
-  fetch (requestURL, {
-    method: 'POST'
-  })
-  .then(res => res.json())
-  .then(res => {
-    if (res.success) {
-      const companies = jsonObject['directory'];  
-      //console.log(JSON.parse(userJson));
-      console.table(JSON.parse(jsonObject));  // temporary checking for valid response and data parsing
-      companies.forEach(displayCompany);
-    }else{
-      console.table("nop");
-    }
-  })
-  .catch(function() {
-    alert("Can't connect to backend try latter");
-  });
-
-function displayCompany(company) {
+  
+function displayTemple(temple) {
   // Create elements to add to the document
   let card = document.createElement('section');
   let portrait = document.createElement('img');
@@ -40,14 +22,14 @@ function displayCompany(company) {
   let link = document.createElement("a");
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
-  portrait.setAttribute('src', company.imageurl);
-  portrait.setAttribute('alt', `Logo of ${company.company}`);
+  portrait.setAttribute('src', temple.imageurl);
+  portrait.setAttribute('alt', `Logo of ${temple.name}`);
   portrait.setAttribute('loading', 'lazy');
 
   // Change the textContent property of the h2 element to contain the company name
-  p.textContent = `${company.address}`;
-  p2.textContent = `${company.phone}`;
-  link.textContent = `${company.website}`;
+  p.textContent = `${temple.name}`;
+  p2.textContent = `${temple.phone}`;
+  link.textContent = `${temple.temple-closure}`;
 
 //`string text ${expression} string text`
   // Add/append the section(card) with the h2 element
