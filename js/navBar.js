e2ee1c2b8d98aba708c9fa3e535f0a7ebaaf7b3e
Your Name <you@example.com>
2022-04-06 12:08:49 -0500

    json file added

--- js/navBar.js
+++ js/navBar.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('#iconBtn').click(function(){
         $('ul').toggleClass('show');
+        const separeteNav = document.getElementById("call");
+        separeteNav.setAttribute("style", "padding-top: 250px;")
     });
 });
